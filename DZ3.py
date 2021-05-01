7edcf8a7ece222aadea06fbe9aca5eef909df2f4
TetyanaMishevska <83297345+TetyanaMishevska@users.noreply.github.com>
2021-05-01 13:27:31 +0300

    Add files via upload

---
+++ DZ3.py
@@ -0,0 +1,5 @@
+stroka1 = str(input('Введите символы первой строки: '))
+stroka2 = ' '
+stroka3 = str(input('Введите символы второй строки: '))
+
+print('Итог конкатенации: ', stroka1 + stroka2 + stroka3)
