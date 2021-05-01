a6128a941ef4b877c151d0d3fe71676b3234351a
TetyanaMishevska <83297345+TetyanaMishevska@users.noreply.github.com>
2021-05-01 13:26:56 +0300

    Add files via upload

---
+++ DZ2.py
@@ -0,0 +1,12 @@
+A = float(input('Enter A:'))
+B = float(input('Enter B:'))
+
+slozhenie = A+B
+vychitanie = A-B
+umnozhenie = A*B
+delenie = A/B
+
+print('A + B = %.2f' % slozhenie)
+print('A - B = %.2f' % vychitanie)
+print('A * B = %.2f' % umnozhenie)
+print('A / B = %.2f' % delenie)
